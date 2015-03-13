a04de5d15069f2e746f111d66e2e7f4b7b004e09
Devin J. Pohly <djpohly@gmail.com>
2015-03-13 13:17:19 -0400

    ok, enough of pointers to points

--- geometry.c
+++ geometry.c
@@ -63,37 +63,45 @@ struct point points_bbox_center(const struct point *pts, int n)
 /*
  * Returns the norm of a point as a vector
  */
-static double point_norm(const struct point *p)
+static double point_norm(struct point p)
 {
-	return p->x*p->x + p->y*p->y;
+	return p.x * p.x + p.y * p.y;
 }
 
 /*
  * According to the original code, "signed area/determinant thing"; let's just
  * stick with that
  */
-static double point_cross(const struct point *p, const struct point *q)
+static double point_cross(struct point p, struct point q)
 {
-	return p->x * q->y - q->x * p->y;
+	return p.x * q.y - q.x * p.y;
+}
+
+/*
+ * Subtracts two points
+ */
+static struct point point_sub(struct point p, struct point q)
+{
+	struct point d;
+	d.x = p.x - q.x;
+	d.y = p.y - q.y;
+	return d;
 }
 
 /*
  * Returns the distance between two points
  */
-static double point_distance(const struct point *p, const struct point *q)
+static double point_distance(struct point p, struct point q)
 {
-	struct point d;
-	d.x = p->x - q->x;
-	d.y = p->y - q->y;
-	return sqrt(point_norm(&d));
+	return sqrt(point_norm(point_sub(q, p)));
 }
 
 /*
  * Determines if a circle contains the given point
  */
-static int circle_contains(const struct circle *c, const struct point *p)
+static int circle_contains(const struct circle *c, struct point p)
 {
-	return point_distance(&c->c, p) <= c->r;
+	return point_distance(c->c, p) <= c->r;
 }
 
 /*
@@ -104,7 +112,7 @@ static int circle_contains_all(const struct circle *c, const struct point *pts,
 {
 	int i;
 	for (i = 0; i < n; i++)
-		if (!circle_contains(c, &pts[i]))
+		if (!circle_contains(c, pts[i]))
 			return 0;
 	return 1;
 }
@@ -112,37 +120,37 @@ static int circle_contains_all(const struct circle *c, const struct point *pts,
 /*
  * Returns the circle defined by a given diameter
  */
-static void circle_from_diameter(const struct point *p, const struct point *q,
+static void circle_from_diameter(struct point p, struct point q,
 		struct circle *c)
 {
-	c->c.x = (p->x + q->x) / 2;
-	c->c.y = (p->y + q->y) / 2;
+	c->c.x = (p.x + q.x) / 2;
+	c->c.y = (p.y + q.y) / 2;
 	c->r = point_distance(p, q) / 2;
 }
 
 /*
  * Returns the circumcircle of three points
  */
-static void circle_circumscribe(const struct point *p, const struct point *q,
-		const struct point *r, struct circle *c)
+static void circle_circumscribe(struct point p, struct point q, struct point r,
+		struct circle *c)
 {
-	//double d = (p->x * (q->y - r->y) + q->x * (r->y - p->y) + r->x * (p->y - q->y)) * 2;
+	//double d = (p.x * (q.y - r.y) + q.x * (r.y - p.y) + r.x * (p.y - q.y)) * 2;
 	double d = (point_cross(p, q) + point_cross(q, r) + point_cross(r, p)) / 2;
 	if (d == 0) {
 		c->c.x = c->c.y = c->r = 0;
 		return;
 	}
-	c->c.x = (point_norm(p) * (q->y - r->y) +
-			point_norm(q) * (r->y - p->y) +
-			point_norm(r) * (p->y - q->y)) / d;
-	c->c.y = (point_norm(p) * (r->x - q->x) +
-			point_norm(q) * (p->x - r->x) +
-			point_norm(r) * (q->x - p->x)) / d;
-	c->r = point_distance(p, &c->c);
+	c->c.x = (point_norm(p) * (q.y - r.y) +
+			point_norm(q) * (r.y - p.y) +
+			point_norm(r) * (p.y - q.y)) / d;
+	c->c.y = (point_norm(p) * (r.x - q.x) +
+			point_norm(q) * (p.x - r.x) +
+			point_norm(r) * (q.x - p.x)) / d;
+	c->r = point_distance(p, c->c);
 }
 
 static void circle_2points(const struct point *pts, int n,
-		const struct point *p, const struct point *q, struct circle *c)
+		struct point p, struct point q, struct circle *c)
 {
 	int i;
 	struct circle temp, temp2;
@@ -154,35 +162,25 @@ static void circle_2points(const struct point *pts, int n,
 		return;
 	}
 
-	struct point pq;
-	pq.x = q->x - p->x;
-	pq.y = q->y - p->y;
+	struct point pq = point_sub(q, p);;
 
 	temp.r = temp2.r = 0;
 	for (i = 0; i < n; i++) {
-		struct point pr;
-		pr.x = pts[i].x - p->x;
-		pr.y = pts[i].y - p->y;
+		struct point pr = point_sub(pts[i], p);;
 
-		double cross = point_cross(&pq, &pr);
+		double cross = point_cross(pq, pr);
 		struct circle cc;
-		circle_circumscribe(p, q, &pts[i], &cc);
+		circle_circumscribe(p, q, pts[i], &cc);
 		if (cc.r == 0)
 			continue;
 
-		struct point pc;
-		pc.x = cc.c.x - p->x;
-		pc.y = cc.c.y - p->y;
-		struct point p1;
-		p1.x = temp.c.x - p->x;
-		p1.y = temp.c.y - p->y;
-		struct point p2;
-		p2.x = temp2.c.x - p->x;
-		p2.y = temp2.c.y - p->y;
+		struct point pc = point_sub(cc.c, p);
+		struct point p1 = point_sub(temp.c, p);
+		struct point p2 = point_sub(temp2.c, p);;
 
-		if (cross > 0 && (temp.r == 0 || point_cross(&pq, &pc) > point_cross(&pq, &p1)))
+		if (cross > 0 && (temp.r == 0 || point_cross(pq, pc) > point_cross(pq, p1)))
 			temp = cc;
-		else if (cross < 0 && (temp2.r == 0 || point_cross(&pq, &pc) < point_cross(&pq, &p2)))
+		else if (cross < 0 && (temp2.r == 0 || point_cross(pq, pc) < point_cross(pq, p2)))
 			temp2 = cc;
 	}
 	if (temp2.r == 0)
